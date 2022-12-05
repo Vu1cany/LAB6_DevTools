6c694c8c6d4e3aed55c75e69084d0b3a5f26dd23
Alexey Nikolaev <leshanikolaev2004@yandex.ru>
2022-12-05 11:46:04 +0300

    no message

---
+++ Tests_for_Console_Calculator/cmake-build-debug/CMakeFiles/Tests.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vu1cany/IT/ITMO/DevTools/LAB6/Tests_for_Console_Calculator/Functions_Tests.cpp" "CMakeFiles/Tests.dir/Functions_Tests.cpp.o" "gcc" "CMakeFiles/Tests.dir/Functions_Tests.cpp.o.d"
+  "/Users/vu1cany/IT/ITMO/DevTools/LAB6/Console_Calculator/Functions.cpp" "CMakeFiles/Tests.dir/Users/vu1cany/IT/ITMO/DevTools/LAB6/Console_Calculator/Functions.cpp.o" "gcc" "CMakeFiles/Tests.dir/Users/vu1cany/IT/ITMO/DevTools/LAB6/Console_Calculator/Functions.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Tests_for_Console_Calculator/cmake-build-debug/CMakeFiles/Tests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tests.dir/Functions_Tests.cpp.o"
+  "CMakeFiles/Tests.dir/Functions_Tests.cpp.o.d"
+  "CMakeFiles/Tests.dir/Users/vu1cany/IT/ITMO/DevTools/LAB6/Console_Calculator/Functions.cpp.o"
+  "CMakeFiles/Tests.dir/Users/vu1cany/IT/ITMO/DevTools/LAB6/Console_Calculator/Functions.cpp.o.d"
+  "Tests"
+  "Tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
